0adee194d3208567f7c2284d3455ca6a34bf0d2e
cristhian vargas <cristhian.a.vargas@gmail.com>
2021-06-26 13:53:37 -0500

    implement the responsiveness for the navbar

--- js/app.js
+++ js/app.js
@@ -5,3 +5,22 @@ const navbar = document.querySelector("#nav");
 window.onscroll = () => {
   this.scrollY > 100 ? nav.classList.add("sticky") : nav.classList.remove("sticky")
 }
+
+// Mobile Menu
+const burgerMenu = document.querySelector(".menu-container")
+const navigationMenu = document.querySelector('.nav-links')
+const navLinks = document.querySelectorAll('.nav-links li')
+
+burgerMenu.addEventListener('click', () => {
+  navigationMenu.classList.toggle('nav-active')
+
+  navLinks.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = "";
+    } else {
+      link.style.animation = `navLinkFade 0.3s ease forwards ${
+        index / 5
+      }s`;
+    }
+  });
+})
